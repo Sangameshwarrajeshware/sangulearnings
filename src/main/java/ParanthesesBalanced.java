1db3103e755040e038077300d297ca2d3e711b91
Sangameshwar R <sangameshwar.r@lastminute.com>
2020-08-05 20:23:47 +0530

    Parantheses problem solved

--- src/main/java/ParanthesesBalanced.java
+++ src/main/java/ParanthesesBalanced.java
@@ -10,24 +10,18 @@ public class ParanthesesBalanced {
     System.out.println(isBalanced("]})"));
   }
 
-  private static boolean isBalanced(String anSetOfParantheses) {
+  public static boolean isBalanced(String anSetOfParantheses) {
     Stack<Character> stack = new Stack<Character>();
     for (int i = 0; i < anSetOfParantheses.length(); i++) {
       char character = anSetOfParantheses.charAt(i);
       if (isOpenParantheses(character)) {
         stack.push(character);
-      } else if (character == ')') {
-        if (stack.empty() || stack.pop() != '(') {
+      } else if (character == ')' && (stack.empty() || stack.pop() != '(')) {
           return false;
-        }
-      } else if (character == ']') {
-        if (stack.empty() || stack.pop() != '[') {
+      } else if (character == ']' && (stack.empty() || stack.pop() != '[')) {
           return false;
-        }
-      } else if (character == '}') {
-        if (stack.empty() || stack.pop() != '{') {
+      } else if (character == '}' && (stack.empty() || stack.pop() != '{')) {
           return false;
-        }
       }
 
     }
